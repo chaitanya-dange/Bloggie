d2264b652be0ff022c20acfe244e1c9a39eed1be
chaitanya-dange <semicd99@gmail.com>
2024-01-26 19:49:44 +0530

    Many to Many relationship between Tag and BlogPost

--- Bloggie.Web/Models/Domain/BlogPost.cs
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -18,7 +18,9 @@
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public bool Visible { get; set; }
-       
 
-    }
+        public ICollection<Tag> Tags { get; set; }
+
+
+}
 }
--- Bloggie.Web/Models/Domain/Tag.cs
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -7,5 +7,7 @@
         public string Name { get; set; }
 
         public string DisplayName { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
     }
 }
