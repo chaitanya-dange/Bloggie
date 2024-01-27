2b3c2c1935c86f669a1185bcec05bcc119544257
chaitanya-dange <semicd99@gmail.com>
2024-01-27 11:24:01 +0530

    Form Submission and DataBinding in mvc

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
@@ -8,6 +9,14 @@ namespace Bloggie.Web.Controllers
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest request)
+        {
+            var name= request.Name;
+            var displayName= request.DisplayName;
+            return View("Add");
         }
     }
 }
---
+++ Bloggie.Web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+
+
+        public string DisplayName { get; set; }
+
+    }
+}
