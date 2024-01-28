dfe89b5e2914cfa4ff30fa9fb8ec571680cbde47
chaitanya-dange <semicd99@gmail.com>
2024-01-28 07:55:44 +0530

    Interface and its implementation structure for Tag

---
+++ Bloggie.Web/Repositories/TagRepository.cs
@@ -0,0 +1,32 @@
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
+{
+    public class TagRepository : ITagInterface
+    {
+        Task<Tag> ITagInterface.AddAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Tag?> ITagInterface.DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<Tag>> ITagInterface.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Tag> ITagInterface.GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Tag?> ITagInterface.UpdateAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
